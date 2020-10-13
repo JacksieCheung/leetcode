4abbe65f6972a0d7f64f25eb1d702ed8a717d3e4
JacksieCheung <1799902904@qq.com>
2020-10-13 10:52:17 +0800

    leetcode 94

---
+++ 94.go
@@ -0,0 +1,26 @@
+package main
+
+// 二叉树中序排序
+// 递归实现
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	var res []int
+	dfs(root, &res)
+	return res
+}
+
+func dfs(root *TreeNode, res *[]int) {
+	if root == nil {
+		return
+	}
+	dfs(root.Left, res)
+	*res = append(*res, root.Val)
+	dfs(root.Right, res)
+}
